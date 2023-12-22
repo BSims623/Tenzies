46f6ca6ea809c8ce7e408f2b4375023c75e08582
Brian Sims <briansims623@gmail.com>
2023-12-21 21:48:33 -0800

    fix: missing die iphone

--- client/src/components/Dice1.jsx
+++ client/src/components/Dice1.jsx
@@ -5,7 +5,7 @@ const Dice1 = (props) => {
     // width="80" height="80"
     return (
         <div className="die" style={props.styles} onClick={() => props.holdDie(props.id)}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-dice-1-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className="bi bi-dice-1-fill" viewBox="0 0 16 16">
                 <path d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3zm5 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
             </svg>
         </div>
